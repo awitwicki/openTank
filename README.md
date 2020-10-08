6778c2bc77028ea0c0340caaac23ff3ed6f6b385
Andrew Witwicki <vitvitskiy3@gmail.com>
2020-10-08 17:36:05 +0200

    ssh control script, readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# openTank
+# openTank - opensource tank platform
+
+<img src="img/img_1.jpg">
+
+## Dependencies
+- [3d printed parts](https://a360.co/33G5BDA)
+- [Plastic Track for Damping Robotic Smart Car Model (1 pcs)](https://www.aliexpress.com/item/32778979359.html?spm=a2g0s.9042311.0.0.27424c4dKxgcJo)
+- Raspberry Pi Zero
+- Raspberry Pi Camera (optional)
+- DC motors
+- L293D
+- 12V Baterry
---
+++ opentank/demotank_ssh.py
@@ -0,0 +1,63 @@
+import curses
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+
+#set GPIO numbering mode and define output pins
+GPIO.setmode(GPIO.BOARD)
+
+motor1a = 8
+motor1b = 10
+
+motor2a = 5
+motor2b = 7
+
+GPIO.setup(motor1a,GPIO.OUT)
+GPIO.setup(motor1b,GPIO.OUT)
+GPIO.setup(motor2a,GPIO.OUT)
+GPIO.setup(motor2b,GPIO.OUT)
+
+# Get the curses window, turn off echoing of keyboard to screen, turn on
+# instant (no waiting) key response, and use special values for cursor keys
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+curses.halfdelay(3)
+screen.keypad(True)
+
+try:
+    while True:
+        char = screen.getch()
+        if char == ord('q'):
+            break
+        elif char == curses.KEY_UP:
+            GPIO.output(motor1a,GPIO.HIGH)
+            GPIO.output(motor1b,GPIO.LOW)
+            GPIO.output(motor2a,GPIO.HIGH)
+            GPIO.output(motor2b,GPIO.LOW)
+        elif char == curses.KEY_DOWN:
+            GPIO.output(motor1a,GPIO.LOW)
+            GPIO.output(motor1b,GPIO.HIGH)
+            GPIO.output(motor2a,GPIO.LOW)
+            GPIO.output(motor2b,GPIO.HIGH)
+        elif char == curses.KEY_RIGHT:
+            GPIO.output(motor1a,GPIO.HIGH)
+            GPIO.output(motor1b,GPIO.LOW)
+            GPIO.output(motor2a,GPIO.LOW)
+            GPIO.output(motor2b,GPIO.HIGH)
+        elif char == curses.KEY_LEFT:
+            GPIO.output(motor1a,GPIO.LOW)
+            GPIO.output(motor1b,GPIO.HIGH)
+            GPIO.output(motor2a,GPIO.HIGH)
+            GPIO.output(motor2b,GPIO.LOW)
+        else:
+            GPIO.output(motor1a,GPIO.LOW)
+            GPIO.output(motor1b,GPIO.LOW)
+            GPIO.output(motor2a,GPIO.LOW)
+            GPIO.output(motor2b,GPIO.LOW)
+
+finally:
+    #Close down curses properly, inc turn echo back on!
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+    GPIO.cleanup()
