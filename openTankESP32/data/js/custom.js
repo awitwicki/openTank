30ada79530eb4bdd500477cef4e0834c5fc39979
Andrew Witwicki <vitvitskiy3@gmail.com>
2021-08-26 22:58:44 +0200

    Add videostream, UI frontend to esp32

--- openTankESP32/data/js/custom.js
+++ openTankESP32/data/js/custom.js
@@ -1,6 +1,9 @@
-var targetUrl = `ws://${window.location.hostname}/ws`;
+var targetUrl = "ws://" + window.location.host + ":82";
+
 var websocket;
 window.addEventListener('load', onLoad);
+
+const view = document.getElementById('stream');
 
 var setr = [0, 0];
 var joy;
@@ -12,45 +15,46 @@ function onLoad() {
   var joyStickParameters = {
     internalFillColor: "#000000",
     internalLineWidth: "#000000",
-    internalStrokeColor: "#AAAAAA",
+    internalStrokeColor: "#999999",
     externalStrokeColor: "#000000",
   };
 
   joy = new JoyStick('joyDiv', joyStickParameters);
 
-  initializeSocket();
+  initializeSockets();
 }
 
-function initializeSocket() {
-  console.log('Opening WebSocket connection to ESP32...');
+function initializeSockets() {
+  console.log('Opening WebSocket connection to OpenTank ESP32...');
+
   websocket = new WebSocket(targetUrl);
-  websocket.onopen = onOpen;
-  websocket.onclose = onClose;
-  websocket.onmessage = onMessage;
-}
 
-function onOpen(event) {
-  console.log('Starting connection to server..');
-  setInterval(loop, 100);
-}
+  websocket.onopen = event => {
+    console.log('Starting connection to server..');
+    setInterval(loop, 100);
+  };
 
-function onClose(event) {
-  console.log('Closing connection to server..');
-  setTimeout(initializeSocket, 2000);
-}
+  websocket.onmessage = message => {
+    if (message.data instanceof Blob) {
+      console.log("WebSocket image received")
+      var urlObject = URL.createObjectURL(message.data);
+      view.src = urlObject;
+    }
 
-function onMessage(event) {
-  console.log("WebSocket message received:", event)
-  var received_msg = evt.data;
-  console.log("received: " + received_msg);
+    d = new Date();
+    var ping = d.getMilliseconds() - time;
+  
+    document.getElementById('stat').innerHTML = 'ping: ' + ping + 'ms';
+  };
 
-  d = new Date();
-  var ping = d.getMilliseconds() - time;
-
-  document.getElementById('stat').innerHTML = 'ping: ' + ping + 'ms';
+  websocket.onclose = message => {
+    console.log('camWebSocket Closing connection to server..');
+    setTimeout(initializeSockets, 2000);
+  };
 }
 
 function loop() {
+  //get data from canvas joy stick
   setr[0] = joy.GetX();
   setr[1] = joy.GetY();
 
--- openTankESP32/src/main.cpp
+++ openTankESP32/src/main.cpp
@@ -1,295 +1,129 @@
-#include <Arduino.h>
+#include <ArduinoWebsockets.h>
 #include <WiFi.h>
-#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "SPIFFS.h"
+#include "esp_camera.h"
 
-/*
-  The resolution of the PWM is 8 bit so the value is between 0-255
-  We will set the speed between 100 to 255.
-*/
-enum speedSettings
-{
-  SLOW = 100,
-  NORMAL = 180,
-  FAST = 255
-};
-
-class Tank
-{
-private:
-  // Motor 1 connections
-  int in1 = 16;
-  int in2 = 17;
-  // Motor 2 connections
-  int in3 = 32;
-  int in4 = 33;
-
-  // PWM Setup to control motor speed
-  const int SPEED_CONTROL_PIN_1 = 25;
-  const int SPEED_CONTROL_PIN_2 = 26;
-  // Play around with the frequency settings depending on the motor that you are using
-  const int freq = 2000;
-  const int channel_0 = 1;
-  const int channel_1 = 2;
-  // 8 Bit resolution for duty cycle so value is between 0 - 255
-  const int resolution = 8;
-
-  // holds the current speed settings, see values for SLOW, NORMAL, FAST
-  speedSettings currentSpeedSettings;
-
-public:
-  Tank()
-  {
-    // Set all pins to output
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    pinMode(SPEED_CONTROL_PIN_1, OUTPUT);
-    pinMode(SPEED_CONTROL_PIN_2, OUTPUT);
-
-    // Set initial motor state to OFF
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-
-    //Set the PWM Settings
-    ledcSetup(channel_0, freq, resolution);
-    ledcSetup(channel_1, freq, resolution);
-
-    //Attach Pin to Channel
-    ledcAttachPin(SPEED_CONTROL_PIN_1, channel_0);
-    ledcAttachPin(SPEED_CONTROL_PIN_2, channel_1);
-
-    // initialize default speed to SLOW
-    setCurrentSpeed(speedSettings::NORMAL);
-  }
-
-  // Turn the Tank left
-  void turnLeft()
-  {
-    Serial.println("Tank is turning left...");
-    setMotorSpeed();
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-  }
-
-  // Turn the Tank right
-  void turnRight()
-  {
-    Serial.println("Tank is turning right...");
-    setMotorSpeed();
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-
-  // Move the Tank forward
-  void moveForward()
-  {
-    Serial.println("Tank is moving forward...");
-    setMotorSpeed();
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-
-  // Move the Tank backward
-  void moveBackward()
-  {
-    setMotorSpeed();
-    Serial.println("Tank is moving backward...");
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  }
-
-  // Stop the Tank
-  void stop()
-  {
-    Serial.println("Tank is stopping...");
-    ledcWrite(channel_0, 0);
-    ledcWrite(channel_1, 0);
-
-    // // Turn off motors
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-  }
-
-  // Set the motor speed
-  void setMotorSpeed()
-  {
-    // change the duty cycle of the speed control pin connected to the motor
-    Serial.print("Speed Settings: ");
-    Serial.println(currentSpeedSettings);
-    ledcWrite(channel_0, currentSpeedSettings);
-    ledcWrite(channel_1, currentSpeedSettings);
-  }
-  // Set the current speed
-  void setCurrentSpeed(speedSettings newSpeedSettings)
-  {
-    Serial.println("Tank is changing speed...");
-    currentSpeedSettings = newSpeedSettings;
-  }
-  // Get the current speed
-  speedSettings getCurrentSpeed()
-  {
-    return currentSpeedSettings;
-  }
-};
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
 // Change this to your network SSID
 const char *ssid = "OpenTank";
 const char *password = "88888888";
 
-// AsyncWebserver runs on port 80 and the asyncwebsocket is initialize at this point also
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
+camera_fb_t *fb = NULL;
 
-// Our Tank object
-Tank tank;
+using namespace websockets;
+WebsocketsServer server;
+AsyncWebServer webserver(80);
 
-// Function to send commands to Tank
-void sendTankCommand(const char *command)
+const int fwdPin = 2;   //Forward Motor Pin
+const int turnPin = 12; //Steering Servo Pin
+
+// Arduino like analogWrite
+// value has to be between 0 and valueMax
+void fwdAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 4000)
 {
-  // command could be either "left", "right", "forward" or "reverse" or "stop"
-  // or speed settingg "slow-speed", "normal-speed", or "fast-speed"
-  if (strcmp(command, "left") == 0)
-  {
-    tank.turnLeft();
-  }
-  else if (strcmp(command, "right") == 0)
-  {
-    tank.turnRight();
-  }
-  else if (strcmp(command, "up") == 0)
-  {
-    tank.moveForward();
-  }
-  else if (strcmp(command, "down") == 0)
-  {
-    tank.moveBackward();
-  }
-  else if (strcmp(command, "stop") == 0)
-  {
-    tank.stop();
-  }
-  else if (strcmp(command, "slow-speed") == 0)
-  {
-    tank.setCurrentSpeed(speedSettings::SLOW);
-  }
-  else if (strcmp(command, "normal-speed") == 0)
-  {
-    tank.setCurrentSpeed(speedSettings::NORMAL);
-  }
-  else if (strcmp(command, "fast-speed") == 0)
-  {
-    tank.setCurrentSpeed(speedSettings::FAST);
-  }
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+  ledcWrite(channel, duty);
+}
+void steeringAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 180)
+{
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+  ledcWrite(channel, duty);
 }
 
-// Processor for index.html page template.  This sets the radio button to checked or unchecked
-String indexPageProcessor(const String &var)
-{
-  String status = "";
-  if (var == "SPEED_SLOW_STATUS")
-  {
-    if (tank.getCurrentSpeed() == speedSettings::SLOW)
-    {
-      status = "checked";
-    }
-  }
-  else if (var == "SPEED_NORMAL_STATUS")
-  {
-    if (tank.getCurrentSpeed() == speedSettings::NORMAL)
-    {
-      status = "checked";
-    }
-  }
-  else if (var == "SPEED_FAST_STATUS")
-  {
-    if (tank.getCurrentSpeed() == speedSettings::FAST)
-    {
-      status = "checked";
-    }
-  }
-  return status;
-}
-
-// Callback function that receives messages from websocket client
-void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-               void *arg, uint8_t *data, size_t len)
-{
-  switch (type)
-  {
-  case WS_EVT_CONNECT:
-  {
-    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-    // client->printf("Hello Client %u :)", client->id());
-    // client->ping();
-  }
-
-  case WS_EVT_DISCONNECT:
-  {
-    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-  }
-
-  case WS_EVT_DATA:
-  {
-    //data packet
-    AwsFrameInfo *info = (AwsFrameInfo *)arg;
-    if (info->final && info->index == 0 && info->len == len)
-    {
-      //the whole message is in a single frame and we got all of it's data
-      if (info->opcode == WS_TEXT)
-      {
-        data[len] = 0;
-        char *command = (char *)data;
-        sendTankCommand(command);
-      }
-    }
-  }
-
-  case WS_EVT_PONG:
-  {
-    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
-  }
-
-  case WS_EVT_ERROR:
-  {
-    // Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
-  }
-  }
-}
-
-// Function called when resource is not found on the server
-void notFound(AsyncWebServerRequest *request)
-{
-  request->send(404, "text/plain", "Not found");
-}
-
-// Setup function
 void setup()
 {
-  // Initialize the serial monitor baud rate
   Serial.begin(115200);
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
 
-//============================================
+  // forward motor PWM
+  ledcSetup(2, 200, 12);    //channel, freq, resolution
+  ledcAttachPin(fwdPin, 2); // pin, channel
+
+  // steering servo PWM
+  ledcSetup(4, 50, 16);      //channel, freq, resolution
+  ledcAttachPin(turnPin, 4); // pin, channel
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if (psramFound())
+  {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_SVGA);
+
+  //WiFi create Access point
   WiFi.softAP(ssid, password);
 
   Serial.print("AP IP Address: ");
   Serial.println(WiFi.softAPIP());
+
+  // Wait some time to connect to wifi
+  for (int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP()); // You can get IP address assigned to ESP
 
   // Initialize SPIFFS
   if (!SPIFFS.begin(true))
@@ -298,36 +132,75 @@ void setup()
     return;
   }
 
-  // Add callback function to websocket server
-  ws.onEvent(onWsEvent);
-  server.addHandler(&ws);
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-              Serial.println("Requesting index page...");
-              request->send(SPIFFS, "/index.html", "text/html", false, indexPageProcessor);
-            });
+  webserver.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Serial.println("Requesting index page...");
+    request->send(SPIFFS, "/index.html", "text/html", false);
+  });
 
   // Route to load entireframework.min.css file
-  server.on("/css/entireframework.min.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/css/entireframework.min.css", "text/css"); });
+  webserver.on("/css/entireframework.min.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/css/entireframework.min.css", "text/css"); });
 
   // Route to load custom.css file
-  server.on("/css/custom.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/css/custom.css", "text/css"); });
+  webserver.on("/css/custom.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/css/custom.css", "text/css"); });
 
   // Route to load custom.js file
-  server.on("/js/custom.js", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/js/custom.js", "text/javascript"); });
+  webserver.on("/js/custom.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/js/custom.js", "text/javascript"); });
 
-  // On Not Found
-  server.onNotFound(notFound);
+  // Route to load custom.js file
+  webserver.on("/js/joy.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/js/joy.js", "text/javascript"); });
 
-  // Start server
-  server.begin();
+  webserver.begin();
+  server.listen(82);
+  Serial.print("Is server live? ");
+  Serial.println(server.available());
+}
+
+void handle_message(WebsocketsMessage msg)
+{
+  int commaIndex = msg.data().indexOf(',');
+  int steerValue = msg.data().substring(0, commaIndex).toInt();
+  int forwardValue = msg.data().substring(commaIndex + 1).toInt();
+
+  if (steerValue > 5)
+  {
+    steeringAnalogWrite(4, 112);
+  }
+  else if (steerValue < -5)
+  {
+    steeringAnalogWrite(4, 65);
+  }
+  else
+  {
+    steeringAnalogWrite(4, 85); // center steering
+  }
+
+  forwardValue = map(forwardValue, 0, -90, 500, 2000);
+
+  if (forwardValue > 600)
+  {
+    fwdAnalogWrite(2, forwardValue);
+  }
+  else
+  {
+    fwdAnalogWrite(2, 0); // stop
+  }
 }
 
 void loop()
 {
-  // No code in here.  Server is running in asynchronous mode
+  //mjpeg camera stream
+  auto client = server.accept();
+  client.onMessage(handle_message);
+  while (client.available())
+  {
+    client.poll();
+    fb = esp_camera_fb_get();
+    client.sendBinary((const char *)fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+    fb = NULL;
+  }
 }
