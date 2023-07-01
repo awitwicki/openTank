94cb393cd7e64c8f60ecd4ae1e2a3919af743a0b
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-07-01 08:32:42 +0200

    Fix styles, cleaning code

--- openTankESP32/data/js/index.js
+++ openTankESP32/data/js/index.js
@@ -1,7 +1,7 @@
 // Use this for tests on local machine without websocket server
-var test = false;
+const test = true;
 
-var targetUrl = "ws://" + window.location.hostname + ":82";
+var targetUrl = `ws://${window.location.hostname}:82`;
 
 // Keyboard
 var pressedKeys = {};
@@ -26,9 +26,47 @@ var ping = 0;
 // Send ws commands
 var commandsSent = [];
 
+// View in fullscreen
+function openFullscreen() {
+  let elem = document.documentElement;
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { // Safari
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE11
+    elem.msRequestFullscreen();
+  }
+}
+
+// Close fullscreen
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { // Safari
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {  // IE11
+    document.msExitFullscreen();
+  }
+}
+
+// Get background click object
+var object = document.getElementById('fullscreenBtn');
+var fullScreen = false;
+
+object.onclick = function(){
+  if (!fullScreen) {
+    openFullscreen();
+  } else {
+    closeFullscreen()
+  }
+
+  fullScreen = !fullScreen;
+};
+
 function onLoad() {
   // Get joystick area width
-  var joystickWidth = document.getElementById("joyDiv").clientWidth;
+  let joystickWidth = document.getElementById("joyDiv").clientWidth;
   joystickWidth /= 2;
 
   joystick = new JoystickController("stick", joystickWidth, 8);
@@ -36,7 +74,7 @@ function onLoad() {
 }
 
 function drawUI(ping, fps) {
-  document.getElementById('stat').innerHTML = 'ping: ' + ping + 'ms, ' + fps +'FPS';
+  document.getElementById('stat').innerHTML = `ping: ${ping}ms, ${fps}FPS`;
 }
 
 function initializeSockets() {
@@ -60,13 +98,12 @@ function initializeSockets() {
         lastFrameTime = dateNow;
 
         // Parse image and show it on page
-        var urlObject = URL.createObjectURL(message.data);
-        view.src = urlObject;
+        view.src = URL.createObjectURL(message.data);
       }
 
       // Calculate ping
       if (commandsSent.length > 0) {
-        var oldestCommandSent = commandsSent[0];
+        let oldestCommandSent = commandsSent[0];
         ping = (dateNow - oldestCommandSent);
 
         // Delete first element
@@ -97,17 +134,17 @@ function loop() {
   // Get data from keyboard ONE KEY PER TIME
   // Keyboard control override joystick
   // WASD = 87, 65, 83, 68
-  if (pressedKeys[87] == true) { // W
+  if (pressedKeys[87]) { // W
     setr[1] = 100;
   }
-  else if (pressedKeys[83] == true) { // S
+  else if (pressedKeys[83]) { // S
     setr[1] = -100;
   }
 
-  if (pressedKeys[65] == true) { // A
+  if (pressedKeys[65]) { // A
     setr[0] = -100;
   }
-  else if (pressedKeys[68] == true) { // D
+  else if (pressedKeys[68]) { // D
     setr[0] = 100;
   }
 
@@ -116,11 +153,9 @@ function loop() {
 
   // Send data to tank
   if (!test) {
-    // check if websocket connected
+    // Check if websocket connected
     if (websocket.readyState == websocket.OPEN) {
       websocket.send(setr);
     }
   }
-
-  console.log(setr);
 }
